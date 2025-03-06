4f1f3809dc7f1bff28364d3c0812653077c1e508
Diego3004a <diegoelcrack3004@gmail.com>
2025-03-06 14:16:50 -0500

    Descripción de los cambios realizados

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -19,23 +19,26 @@ function Dashboard() {
 
   return (
     <div className="dashboard-container">
-        <header className="dashboard-header">
+      <header className="dashboard-header">
       </header>
       <div className="dashboard-sidebar">
         <div className="sidebar-header">
-        <div className="imagen-header">
-        <img clasName="img-logo" src="../images/ovedisimos-dashboard.png" alt="Imagen de login" /></div>
+          <div className="imagen-header">
+            <img className="img-logo" src="../images/ovedisimos-dashboard.png" alt="Imagen de login" />
+          </div>
         </div>
         <nav>
           <ul className="main-menu">
             <li className={`main-menu-item ${expanded.panel ? 'expanded' : ''}`}>
               <div className="menu-item-header" onClick={() => toggleSection('panel')}>
+              <img src="/images/gear.png" alt="Reportes" style={{ width: '22px', height: '22px' }} />
                 <i className="fas fa-shopping-cart"></i>
-                <span style={{ fontWeight: 'bold' }}>Panel Control</span>
+                <span style={{ fontWeight: 400, fontSize: 18 }}>Panel Control</span>
                 <i className={`fas fa-chevron-${expanded.panel ? 'down' : 'right'}`}></i>
               </div>
               {expanded.panel && (
                 <ul className="submenu">
+                  <li>Pedidos</li>
                   <li>Productos</li>
                   <li>Categorias</li>
                   <li>Almacen</li>
@@ -46,25 +49,57 @@ function Dashboard() {
                 </ul>
               )}
             </li>
-            <li className="main-menu-item">
+            <li className={`main-menu-item ${expanded.clientes ? 'expanded' : ''}`}>
               <div className="menu-item-header" onClick={() => toggleSection('clientes')}>
-                <span style={{ fontWeight: 'bold' }}>Clientes</span>
+              <img src="/images/folder.png" alt="Reportes" style={{ width: '22px', height: '22px' }} />
+                <i className="fas fa-users"></i> 
+                <span style={{ fontWeight: 400, fontSize: 18 }}>Clientes</span>
                 <i className={`fas fa-chevron-${expanded.clientes ? 'down' : 'right'}`}></i>
               </div>
-              {expanded.clientes && <ul className="submenu"></ul>}
+              {expanded.clientes && <ul className="submenu">
+                <li>Cliente 1</li>
+                <li>Cliente 2</li>
+                </ul>}
             </li>
-            <li className="main-menu-item">
+            <li className={`main-menu-item ${expanded.motorizados ? 'expanded' : ''}`}>
               <div className="menu-item-header" onClick={() => toggleSection('motorizados')}>
-              <span style={{ fontWeight: 'bold' }}>Motorizados</span>
+              <img src="/images/file.png" alt="Reportes" style={{ width: '22px', height: '22px' }} />
+                <i className="fas fa-motorcycle"></i> 
+                <span style={{ fontWeight: 400, fontSize: 18 }}>Motorizados</span>
                 <i className={`fas fa-chevron-${expanded.motorizados ? 'down' : 'right'}`}></i>
               </div>
-              {expanded.motorizados && <ul className="submenu"></ul>}
+              {expanded.motorizados && <ul className="submenu">
+                <li>Motorizado 1</li>
+                <li>Motorizado 2</li>
+                </ul>}
+            </li>
+            <li className={`main-menu-item ${expanded.asesores ? 'expanded' : ''}`}>
+              <div className="menu-item-header" onClick={() => toggleSection('asesores')}>
+              <img src="/images/tv.png" alt="Asesores" style={{ width: '22px', height: '22px' }} />
+                <i className="fas fa-user-tie"></i> 
+                <span style={{ fontWeight: 400, fontSize: 18 }}>Asesores</span>
+                <i className={`fas fa-chevron-${expanded.asesores ? 'down' : 'right'}`}></i>
+              </div>
+              {expanded.asesores && <ul className="submenu">
+                <li>Asesor 1</li>
+                <li>Asesor 2</li>
+                </ul>}
+            </li>
+            <li className={`main-menu-item ${expanded.reportes ? 'expanded' : ''}`}>
+            <div className="menu-item-header" onClick={() => toggleSection('reportes')}>
+            <img src="/images/report.png" alt="Reportes" style={{ width: '22px', height: '22px' }} />
+            <i className="fas fa-user-tie"></i> 
+              <span style={{ fontWeight: 400, fontSize: 18 }}>Reportes</span>
+              <i className={`fas fa-chevron-${expanded.reportes ? 'down' : 'right'}`}></i>
+              </div>
+              {expanded.reportes && <ul className="submenu">
+                <li>Reporte 1</li>
+                <li>Reporte 2</li>
+                </ul>}
             </li>
           </ul>
         </nav>
       </div>
-      
-       
     </div>
   );
 }
