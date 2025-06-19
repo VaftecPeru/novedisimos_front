294218cdacae1562f3560dc65cc9b987e557cd34
Christopher <sullcaramirezz@gmail.com>
2025-06-19 12:43:34 -0500

    Update conexionAPI local

--- src/components/services/shopifyService.js
+++ src/components/services/shopifyService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
 const API_BASE_URL = isDevelopment 
-  ? 'http://localhost:3000/api/shopify' 
+  ? 'http://localhost:8000/api/shopify' 
   : 'https://api.novedadeswow.com/api/shopify';
 
 axios.interceptors.response.use(
