d628ef857616a08e38464604646779733fcafc82
Criss2321 <sullcaramirezz@gmail.com>
2025-07-01 15:55:52 -0500

    Estados-pedidos

--- src/components/services/shopifyService.js
+++ src/components/services/shopifyService.js
@@ -1,10 +1,71 @@
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
-const API_BASE_URL = isDevelopment 
-  ? 'http://localhost:8000/api/shopify' 
-  : 'https://api.novedadeswow.com/api/shopify';
+const CUSTOM_API_BASE_URL = isDevelopment
+  ? 'http://localhost:8000/api'
+  : 'https://api.novedadeswow.com/api';
+
+
+  const API_BASE_URL = isDevelopment 
+    ? 'http://localhost:8000/api/shopify' 
+    : 'https://api.novedadeswow.com/api/shopify';
+
+
+
+export const fetchEstadosPedidos = async () => {
+  try {
+    const res = await axios.get(`${CUSTOM_API_BASE_URL}/estado-pedido-todos`);
+    return res.data.data || [];
+  } catch (error) {
+    console.error('Error al obtener estados internos de pedidos:', error);
+    return [];
+  }
+};
+
+export const actualizarEstadoInternoPago = async (pedidoId, estadoPago) => {
+  try {
+    const response = await axios.post(`${CUSTOM_API_BASE_URL}/estado-pedido`, {
+      shopify_order_id: pedidoId,
+      estado_pago: estadoPago,
+    });
+
+    if (response.data && response.data.message) {
+      Swal.fire('Actualizado', 'El estado de pago ha sido actualizado.', 'success');
+    } else {
+      Swal.fire('Error', 'No se pudo actualizar el estado de pago.', 'error');
+    }
+    return response; // <-- importante para el frontend
+  } catch (error) {
+    console.error(error);
+    Swal.fire('Error', 'Error del servidor al actualizar estado de pago.', 'error');
+    return null;
+  }
+};
+
+export const actualizarEstadoInternoPreparacion = async (pedidoId, estadoActual) => {
+  const nuevoEstado = estadoActual === 'preparado' ? 'no_preparado' : 'preparado';
+
+  try {
+    const response = await axios.post(`${CUSTOM_API_BASE_URL}/estado-pedido`, {
+      shopify_order_id: pedidoId,
+      estado_preparacion: nuevoEstado,
+    });
+
+    if (response.data && response.data.message) {
+      Swal.fire('Actualizado', 'El estado de preparación ha sido actualizado.', 'success');
+    } else {
+      Swal.fire('Error', 'No se pudo actualizar el estado de preparación.', 'error');
+    }
+    return response; // <-- AGREGA ESTA LÍNEA
+  } catch (error) {
+    console.error(error);
+    Swal.fire('Error', 'Error del servidor al actualizar estado de preparación.', 'error');
+    return null; // <-- AGREGA ESTA LÍNEA
+  }
+};
+
 
 axios.interceptors.response.use(
   response => response,
